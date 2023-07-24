af8ef1ab135eac4fcf9e685e2ca186af64aa933b
roc102 <kyroc102@gmail.com>
2023-07-24 17:11:59 +0530

    update

--- server.js
+++ server.js
@@ -63,7 +63,6 @@ app.post('/register',(req,res) => {
         id : '125',
         name : name,
         email : email,
-        password : password,
         entries : 0,
         joined : new Date()
     })
